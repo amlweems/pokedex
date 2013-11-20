4898daf082d765d2d6be6803e99d4d86a15480c9
Anthony Weems <amlweems@gmail.com>
2013-11-19 23:26:12 -0600

    initial commit of motd pokedex

---
+++ pokedex.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+
+unlink $HOME/.motd;
+
+POKEMON=$(printf %03d $((RANDOM%119+1)))
+
+# Link motd to 
+ln -vs $SCRIPTPATH/$POKEMON.txt $HOME/.motd
